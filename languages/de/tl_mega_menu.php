350b2b4042fe8a1845d326839b7a6caca667e60d
Martin Schwenzer <mail@derhaeuptling.com>
2017-02-13 21:29:51 +0100

    added german localisation

---
+++ languages/de/tl_mega_menu.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * mega_menu extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2016 derhaeuptling
+ *
+ * @author  derhaeuptling <https://derhaeuptling.com>
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @license LGPL
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_mega_menu']['tstamp']   = ['Änderungsdatum'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['name']     = ['Name', 'Geben Sie den Menü Namen an.'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['template'] = ['Template', 'Wählen Sie hier das Menü Template.'];
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_mega_menu']['name_legend'] = 'Name und Template';
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_mega_menu']['hint'] = 'Stellen Sie sicher, dass Sie eines der Mega Menü Templates als Navigations-Menü auswählen.';
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_mega_menu']['new']        = ['Neues Menü', 'Ertelle ein neues Menü'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['show']       = ['Menü Details', 'Menü Details zu ID %s anzeigen'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['edit']       = ['Edit Menü', 'Menü ID %s editieren'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['editheader'] = ['Menü Einstellungen editieren', 'Menü Einstellungen editieren ID %s'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['copy']       = ['Menü kopieren', 'Kopiere das Menü ID %s'];
+$GLOBALS['TL_LANG']['tl_mega_menu']['delete']     = ['Menü Löschen', 'Lösche das Menü ID %s'];
---
+++ languages/de/tl_page.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * mega_menu extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2016 derhaeuptling
+ *
+ * @author  derhaeuptling <https://derhaeuptling.com>
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @license LGPL
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['megamenu_enable'] = ['Mega Menü aktivieren', 'Aktivieren Sie das Mega Menü für diese Seite.'];
+$GLOBALS['TL_LANG']['tl_page']['megamenu_menu']   = ['Mega Menü', 'Wähle das Mega Menü für diese Seite.'];
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_page']['megamenu_legend'] = 'Mega Menü Einstellungen';
